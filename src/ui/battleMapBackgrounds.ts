172f98de6084928c5733af12e02b826a55581a2b
P.Rata <rataplaian@gmail.com>
2026-09-24 13:00:25 +0200

    Use JPEG battle map assets

--- src/ui/battleMapBackgrounds.ts
+++ src/ui/battleMapBackgrounds.ts
@@ -2,10 +2,10 @@ import { ImageSourcePropType } from 'react-native';
 import { GameMode } from '../game/types';
 
 export const APPROVED_HORIZONTAL_BATTLEMAP: ImageSourcePropType =
-  require('../../assets/maps/nexus-rush-horizontal.png');
+  require('../../assets/maps/nexus-rush-horizontal.jpg');
 
 export const APPROVED_VERTICAL_BATTLEMAP: ImageSourcePropType =
-  require('../../assets/maps/nexus-rush-vertical.png');
+  require('../../assets/maps/nexus-rush-vertical.jpg');
 
 export function mapBackground(mode: GameMode): ImageSourcePropType {
   return mode === 'horizontal-dual-nexus'
