67d47ebb31601005896b6738eb57a17def942eae
P.Rata <rataplaian@gmail.com>
2026-09-24 12:38:32 +0200

    Align logical terrain grids with approved battle maps

--- src/game/maps.ts
+++ src/game/maps.ts
@@ -12,16 +12,17 @@ export const HORIZONTAL_VALLEY: MapDefinition = {
   width: 12,
   height: 8,
   deploymentRows: 2,
-  description: 'Una valle simmetrica con lago centrale, dorsali laterali e colline che dominano gli accessi.',
+  description:
+    'Arena larga con un canale centrale, due attraversamenti principali, coperture compatte e colline laterali. La griglia segue il disegno della battle map illustrata.',
   terrain: [
-    [P,P,P,P,P,P,P,P,P,P,P,P],
-    [P,P,H,P,P,P,P,P,P,H,P,P],
-    [P,M,H,P,P,P,P,P,P,H,M,P],
-    [P,M,P,P,H,W,W,H,P,P,M,P],
-    [P,M,P,P,H,W,W,H,P,P,M,P],
-    [P,M,H,P,P,P,P,P,P,H,M,P],
-    [P,P,H,P,P,P,P,P,P,H,P,P],
-    [P,P,P,P,P,P,P,P,P,P,P,P]
+    [P, P, P, P, P, P, P, P, P, P, P, P],
+    [P, H, P, P, P, W, W, P, P, P, H, P],
+    [P, M, H, P, P, P, P, P, P, H, M, P],
+    [P, P, M, P, P, W, W, P, P, M, P, P],
+    [P, P, M, P, P, W, W, P, P, M, P, P],
+    [P, M, H, P, P, P, P, P, P, H, M, P],
+    [P, H, P, P, P, W, W, P, P, P, H, P],
+    [P, P, P, P, P, P, P, P, P, P, P, P]
   ]
 };
 
@@ -32,21 +33,22 @@ export const VERTICAL_PASS: MapDefinition = {
   width: 8,
   height: 12,
   deploymentRows: 2,
-  description: 'Un passo montano lungo e stretto: due dorsali incanalano l’avanzata intorno a un bacino centrale.',
+  description:
+    'Arena verticale con fiume trasversale, ponte centrale, blocchi di rovine compatti e posizioni elevate simmetriche. La griglia segue il disegno della battle map illustrata.',
   terrain: [
-    [P,P,P,P,P,P,P,P],
-    [P,H,P,P,P,P,H,P],
-    [P,H,M,P,P,M,H,P],
-    [P,P,M,P,P,M,P,P],
-    [P,P,M,H,H,M,P,P],
-    [P,P,P,W,W,P,P,P],
-    [P,P,P,W,W,P,P,P],
-    [P,P,M,H,H,M,P,P],
-    [P,P,M,P,P,M,P,P],
-    [P,H,M,P,P,M,H,P],
-    [P,H,P,P,P,P,H,P],
-    [P,P,P,P,P,P,P,P]
+    [P, P, P, P, P, P, P, P],
+    [P, H, P, P, P, P, H, P],
+    [P, M, H, P, P, H, M, P],
+    [P, M, M, P, P, M, M, P],
+    [P, P, M, P, P, M, P, P],
+    [W, W, W, P, P, W, W, W],
+    [W, W, W, P, P, W, W, W],
+    [P, P, M, P, P, M, P, P],
+    [P, M, M, P, P, M, M, P],
+    [P, M, H, P, P, H, M, P],
+    [P, H, P, P, P, P, H, P],
+    [P, P, P, P, P, P, P, P]
   ]
 };
 
-export const MAPS = [HORIZONTAL_VALLEY, VERTICAL_PASS];
+export const MAPS: MapDefinition[] = [HORIZONTAL_VALLEY, VERTICAL_PASS];
